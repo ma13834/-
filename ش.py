f0a940606bdaeaf8ba1561e040d6b1838c55c0cb
ma13834 <61782175+ma13834@users.noreply.github.com>
2020-09-04 00:46:06 +0430

    aaa

---
+++ ش.py
@@ -0,0 +1,28 @@
+from tabulate import tabulate
+import random
+import time
+
+placement = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+i = 0
+
+while i <10 :
+    names = [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
+    ]
+    names[random.randint(-1, 9)][random.randint(-1, 9)] = 'c'
+    names[random.randint(-1, 9)][random.randint(-1, 9)] = 'r'
+    print(tabulate(names, tablefmt='grid'))
+    print('NEXT TABLE')
+    time.sleep(2)
+    i = i + 1
+import os
+os.system("cls")
